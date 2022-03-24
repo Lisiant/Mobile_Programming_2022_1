083cb5e9e672d3110a05064a41b1e2f00a2ca834
Lisiant <wkdxod321@gmail.com>
2022-03-24 15:58:51 +0900

    week3 mobile programming practice

---
+++ App02/app/src/main/java/com/example/app02_imgselect/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.app02_imgselect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioGroup
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    fun init(){
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        radioGroup.setOnCheckedChangeListener{radioGroup,checkedID->
+            when(checkedID){
+                R.id.radioButton1 -> imageView.setImageResource(R.drawable.img1)
+                R.id.radioButton2 -> imageView.setImageResource(R.drawable.img2)
+                R.id.radioButton3 -> imageView.setImageResource(R.drawable.img3)
+            }
+        }
+    }
+}
