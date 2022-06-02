23fc24f5e24b6e3e7357fbf6b2072916db2f3eb6
Lisiant <wkdxod321@gmail.com>
2022-06-02 11:16:22 +0900

    week14 BroadcastReceiver

---
+++ App28_BroadcastReceiver/app/src/main/java/com/example/app28_broadcastreceiver/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app28_broadcastreceiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
